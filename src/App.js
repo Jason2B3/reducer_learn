43fe900d9393601d8edf37e921754dfa72b3f9da
jason <2B3>
2021-08-03 15:34:27 -0400

    commit 0: operational but w/ bad practices

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useReducer } from "react";
+export default App;
 
+function reducer(state, action) {
+  console.log(state)
+  console.log(action)
+  // Increment or decrement our current count by 1
+  if (action.type === "increment") return { count: state.count + 1 };
+  if (action.type === "decrement") return { count: state.count - 1 };
+  // If none of the above scenarios apply, return the state as is
+  return state
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const plusOne = (prevCount) => dispatch({ type: "increment" });
+  const minusOne = (prevCount) => dispatch({ type: "decrement" });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <button onClick={minusOne}>-</button>
+      <span>{state.count}</span>
+      <button onClick={plusOne}>+</button>
+      <span></span>
+    </React.Fragment>
   );
 }
 
-export default App;
+
