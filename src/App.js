cbdc680a4b6544b9a16c49bb8bf47fd1e83277d7
jason <2B3>
2021-08-04 01:31:59 -0400

    commit 3: payloads

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import "./App.css";
 import React, { useState, useReducer } from "react";
 export default App;
-const ACTIONS = {
-  INCREMENT: "increment",
-  DECREMENT: " decrement",
-};
-function reducer(state, action) {
-  switch(action.type){
-    case ACTIONS.INCREMENT:
-      return { count: state.count + 1 }
-    case ACTIONS.DECREMENT:
-      return { count: state.count - 1 }; 
-    default:
-      return state
+
+function reducer(todos, action) {
+  //% If action.type is ADD_LIST_ITEM, add an object entry to our todo array
+  if (action.type === "ADD_LIST_ITEM") {
+    return [...todos, { name: action.payload.name, id: Math.random() }];
   }
+  //% If the action.type is something else, return the todo array as is
+  return todos;
 }
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
-  const plusOne = (prevCount) => dispatch({ type: ACTIONS.INCREMENT });
-  const minusOne = (prevCount) => dispatch({ type: ACTIONS.DECREMENT });
+  const [todos, dispatch] = useReducer(reducer, []);
+  //% Will give userInput to the reducerFn as its name KVP in the payload
+  // Also, userInput's double binded to the input element
+  const [userInput, setUserInput] = useState("");
+
+  //% Call reducerFn and give it the stateful userInput variable through the payload
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_LIST_ITEM",
+      payload: { name: userInput }, 
+      //$ Payload (must give stateful userInput variable to reducerFn)
+      //$ Cannot directly access it b/c the reducerFn is defined outside the App component ƒ()
+    });
+    console.log(todos);
+    setUserInput(""); // clear input field
+  };
   return (
     <React.Fragment>
-      <button onClick={minusOne}>-</button>
-      <span>{state.count}</span>
-      <button onClick={plusOne}>+</button>
-      <span></span>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)} // Quick double bind
+        />
+      </form>
+      {todos.map((todo) => {
+        return <li key={todo.id}>{todo.name}</li>;
+      })}
     </React.Fragment>
   );
 }
